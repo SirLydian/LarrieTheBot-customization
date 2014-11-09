86aac63933987f136623b690cef7daa5760f12a3
SirLydian <ruben.pauwels@student.howest.be>
2014-11-09 16:39:22 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -31,20 +31,88 @@
          }
 
          */
-
-        bot.commands.baconCommand = {
-            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+        //function DisableAudioVideo() {
+          //  $("div.item.s-av").click();
+       //}
+        
+        //Mehs to skip function
+        var isRecent = false;
+        var mehLimit = Infinity;
+        function callback() {
+            if (API.getScore().negative < mehLimit || isRecent) return;
+            isRecent = true;
+            API.sendChat("@" + API.getDJ().username + " This song will be skipped because it reached the meh limit of "+mehLimit+"!");
+            API.moderateForceSkip();
+            setTimeout(function(){
+               isRecent = false;
+            }, 10000);
+        }
+        API.on(API.VOTE_UPDATE, callback);
+        API.on(API.ADVANCE, function(obj){
+            var WaitlistCount = API.getWaitList().length;
+            if(WaitlistCount <= 45){
+                var mehSkipCount = Math.round((WaitlistCount / 3) + 2);
+                if(mehSkipCount < 5){
+                    mehSkipCount = 5;
+                }
+            }
+            mehLimit = mehSkipCount;
+            setTimeout(function(){
+                API.sendChat("Mehs to skip: " + mehLimit);
+            }, 1500);
+        });
+        
+        function checkManagers(){
+            var managerPresent = false;
+            var staff = API.getStaff();
+            for(var i = 0; i < staff.length; i++){
+                if(staff[i].role === 3 && staff[i].id !== 5152412){
+                    managerPresent = true;
+                }
+            }
+            var newSetting = true;
+            if(managerPresent){
+                newSetting = false;
+            }
+            if(bot.settings.bouncerPlus !== newSetting){
+                if(newSetting){
+                    API.sendChat("Bouncer+ enabled!");
+                    //als je bouncerplus aanzet
+                }else{
+                    API.sendChat("Bouncer+ disabled!");
+                    //als uitzet
+                }
+                bot.settings.bouncerPlus = newSetting;
+            }
+        }
+        
+        //check manager for bouncer+
+        API.on(API.USER_JOIN, checkManagers);
+        API.on(API.USER_LEAVE, checkManagers);
+        
+        bot.commands.testCommand = {
+            command: 'penis',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon!!!");
+                    API.sendChat('@'+chat.un+' Command too short :trollface: !');
+                    //API.sendChat("Command too short :trollface:");
                 }
             }
         };
-
+        
+        //$("div.item.s-av").click();
+        setTimeout(function(){$("div.info").click();}, 2000);
+        setTimeout(function(){$("div.item.settings").click();}, 2800);
+        setTimeout(function(){$("div.item.s-av").click();}, 3600);
+        setTimeout(function(){$("div.back").click();}, 4400);
+        
+        //bot.commands.decommanddagewiledittenCommand.rank = 'user';
+        bot.commands.kickCommand.rank = 'host';
+        
         //Load the chat package again to account for any changes
         bot.loadChat();
 
@@ -53,29 +121,28 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "Larrie The Bot",
         language: "english",
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-        maximumAfk: 120,
+        chatLink: "https://rawgit.com/SirLydian/basicBot/master/lang/en.json",
+        maximumAfk: 60,
         afkRemoval: true,
         maximumDc: 60,
-        bouncerPlus: true,
+        bouncerPlus: false,
         lockdownEnabled: false,
         lockGuard: false,
         maximumLocktime: 10,
         cycleGuard: true,
         maximumCycletime: 10,
         timeGuard: true,
-        maximumSongLength: 10,
+        maximumSongLength: 7,
         autodisable: true,
-        commandCooldown: 30,
+        commandCooldown: 0,
         usercommandsEnabled: true,
-        lockskipPosition: 3,
+        lockskipPosition: 2,
         lockskipReasons: [
             ["theme", "This song does not fit the room theme. "],
             ["op", "This song is on the OP list. "],
             ["history", "This song is in the history. "],
-            ["mix", "You played a mix, which is against the rules. "],
             ["sound", "The song you played had bad sound quality or no sound. "],
             ["nsfw", "The song you contained was NSFW (image or sound). "],
             ["unavailable", "The song you played was not available for some users. "]
@@ -87,21 +154,17 @@
         motd: "Temporary Message of the Day",
         filterChat: true,
         etaRestriction: false,
-        welcome: true,
-        opLink: null,
-        rulesLink: null,
-        themeLink: null,
-        fbLink: null,
-        youtubeLink: null,
-        website: null,
+        welcome: false,
+        opLink: "https://goo.gl/sF5Lma",
+        rulesLink: "https://goo.gl/roLwJ",
+        themeLink: "We only allow EDM Trap and the sub-genres of trap (such as trapstyle, Festival, neuro trap, chill-trap, future bass/beats, Jersey Club, dirty south, trill trap)!",
+        fbLink: "https://www.facebook.com/OfficialTrapCity",
+        youtubeLink: "http://youtube.com/trapcity",
+        website: "https://trapplug.eu/",
         intervalMessages: [],
         messageInterval: 5,
-        songstats: true,
-        commandLiteral: "!",
-        blacklists: {
-            NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-            OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
-        }
+        songstats: false,
+        commandLiteral: "!"
     }));
 
     //Start the bot and extend it when it has loaded.
