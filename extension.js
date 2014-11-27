5d945d7237539c1b01bf6b230d4a43eda2d3bcd6
SirLydian <ruben.pauwels@student.howest.be>
2014-11-27 12:05:48 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -45,6 +45,19 @@
 
          */
          
+        bot.commands.commandCommand = {
+        command: 'niels',
+        rank: 'user',
+        type: 'exact',
+        functionality: function(chat, cmd){
+        if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+	         if( !bot.commands.executable(this.rank, chat) ) return void (0);
+		         else{
+			         API.sendChat("Niels is awesome!");
+		         }
+	         }
+        }
+         
          
         bot.commands.demoteAfkCommand = {
         command: 'afk',
