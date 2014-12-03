3caee37779df352bcf55ca065cb521cf936cbc11
SirLydian <ruben.pauwels@student.howest.be>
2014-12-03 11:47:47 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -52,7 +52,7 @@
         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 	         if( !bot.commands.executable(this.rank, chat) ) return void (0);
 		         else{
-			         API.sendChat("v1.0.0");
+			         API.sendChat("v1.0.1");
 		         }
 	         }
         }
