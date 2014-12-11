9b70747bc12a9fb1af14f8a898ee4850bcf3b30e
SirLydian <ruben.pauwels@student.howest.be>
2014-12-11 09:07:20 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -64,7 +64,7 @@
          
         bot.commands.slotCommand = {
         command: 'slots',
-        rank: 'user',
+        rank: 'host',
         type: 'exact',
         functionality: function(chat, cmd){
         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
