9d03e735fa4b18a065e103c7db62a79d7b846449
SirLydian <ruben.pauwels@student.howest.be>
2015-05-27 01:04:35 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -82,6 +82,7 @@
             var usersNow = API.getUsers().length; //5
             if(usersNow > localStorage.getItem("maxUsers")){
                 localStorage.setItem("maxUsers", usersNow);
+                console.log("New max users record: "+localStorage.getItem("maxUsers")+" !");
             }
         }
         
@@ -97,6 +98,21 @@
                 }
             }
         };
+        
+        var cars = ["Saab", "Volvo", "BMW"];
+        
+        bot.commands.diffPeopleCommand = {
+        command: 'diffpeople',
+        rank: 'manager',
+        type: 'exact',
+        functionality: function(chat, cmd){
+        if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+             if( !bot.commands.executable(this.rank, chat) ) return void (0);
+                 else{
+                     
+                 }
+             }
+        }
 
         bot.commands.tastyplugCommand = {
         command: 'tastyplug',
