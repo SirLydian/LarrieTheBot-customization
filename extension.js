4c35df201007d5607f5e59e44d5b89fdfc4a273c
SirLydian <ruben.pauwels@student.howest.be>
2015-05-30 18:53:04 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -124,8 +124,8 @@
         */
         
         bot.commands.eventcountdownCommand = {
-            command: 'eventcountdown',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'bouncer', //Minimum user permission to use the command
+            command: 'event',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
