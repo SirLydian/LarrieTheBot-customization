d3b6d9ef50d5ce4253c4f8993693168ed99531ed
SirLydian <ruben.pauwels@student.howest.be>
2015-05-30 18:55:52 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -131,7 +131,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    var eventTime = 1433008888363;
+                    var eventTime = 1433008800000;
                     var currentTime = Date.now();
                     var timeUntil = eventTime - currentTime;
                     var time = msToStr(timeUntil);
